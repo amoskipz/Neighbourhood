5451ab5f8a7255ff5dfe3fea7bb92a07f044fd1d
kadas36 <oduorthomas14@gmail.com>
2021-04-11 13:03:08 +0300

    first commit

---
+++ amos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import neighbourhood, Profile, Authorities, Health, Post
+
+
+
+
+
+# Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Authorities)
+admin.site.register(Health)
+admin.site.register(Post)
---
+++ amos/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from .models import neighbourhood
+from django.contrib.auth.models import User
+# Create your tests here.
+
